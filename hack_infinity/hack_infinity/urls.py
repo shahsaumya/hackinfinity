558910602d8e36ba06845c3e43b4856ada28b1e1
anantjoshi1324@gmail.com <Anant Joshi>
2017-11-12 04:11:49 +0530

    Changes

--- hack_infinity/hack_infinity/urls.py
+++ hack_infinity/hack_infinity/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^register/', views.register),
     url(r'^login/', views.login_user),
+    url(r'^add-produce/', views.add_produce)
 ]
