0099ed0c3822d104d9c399fb9a4bee282e4d9f28
Saumya Shah <shahsaumya0108@gmail.com>
2017-11-11 17:34:40 +0530

    Translator model

--- hack_infinity/app/admin.py
+++ hack_infinity/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from app import models
 
+admin.site.register(models.State_translator)
 # Register your models here.
--- hack_infinity/app/models.py
+++ hack_infinity/app/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class State_translator(models.Model):
+    state_name = models.CharField(max_length=50, default=None)
+    state_language = models.CharField(max_length=50, default=None)
+    state_code = models.CharField(max_length=20, default="en")
+
+    def __str__(self):
+        return self.state_name
+
+    def to_dict(self):
+        return convert_to_dict(self)
